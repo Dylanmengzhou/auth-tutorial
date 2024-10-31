56b0344507ebc0b42cd6b5a3fae27fed08295497
bimengzhou <dylanmengzhou@gmail.com>
2024-11-01 00:01:24 +0900

    version 1.0.4: Complete User button in setting page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,19 @@
-import { auth, signOut } from "@/auth";
-const SettingsPage = async () => {
-  const session = await auth();
+"use client";
 
+import { useCurrentUser } from "@/hooks/use-current-user";
+// import { logout } from "@/actions/logout";
+import {signOut } from "next-auth/react";
+
+const SettingsPage = () => {
+  const user = useCurrentUser();
+  const onClick = () => {
+    signOut();
+  };
   return (
-    <div className="">
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: "/auth/login" });
-        }}
-      >
-        <button type="submit">Log out</button>
-      </form>
+    <div className="bg-white p-10 rounded-xl">
+        <button type="submit" onClick={onClick}>
+          Log out
+        </button>
     </div>
   );
 };
